7a98c1f4b1b9094f1d0ed4fc8c8d24cc335faad7
Peris <perisdevs@gmail.com>
2022-07-03 21:56:36 -0700

    Added armors

---
+++ armorRouter.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import fs from 'fs';
+
+const router = express.Router();
+
+export function armorRouter() {
+
+    let armorTypes = fs.readdirSync('data/armors');
+
+    armorTypes.forEach((type) => {
+        router.get(`/${type}`, (req, res) => {
+            let armorFiles = fs.readdirSync(`data/armors/${type}`);
+            let armors = [];
+            armorFiles.forEach((file) => {
+                let armor = JSON.parse(fs.readFileSync(`data/armors/${type}/${file}`));
+                armors.push(armor);
+            });
+            res.json(armors);
+        });
+
+        router.get(`/${type}/:name`, (req, res) => {
+            let file = req.params.name.replace(/\s/g, '-');
+            let armorFile = JSON.parse(fs.readFileSync(`data/armors/${type}/${file}.json`));
+            res.json(armorFile);
+        });
+    });
+
+    router.get('/', (req, res) => {
+        let armors = [];
+        let armorTypes = fs.readdirSync('data/armors');
+
+        armorTypes.forEach((type) => {
+
+            let armorFiles = fs.readdirSync(`data/armors/${type}`);
+
+            armorFiles.forEach((file) => {
+
+                let armor = JSON.parse(fs.readFileSync(`data/armors/${type}/${file}`));
+                armors.push(armor);
+            });
+
+            res.json(armors);
+        });
+    })
+
+    return router;
+}
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { armorRouter } from './armorRouter.js';
 import { weaponRouter } from './weaponRouter.js';
 
 const port = 3000;
@@ -6,6 +7,7 @@ const port = 3000;
 const app = express();
 
 app.use('/weapons', weaponRouter());
+app.use('/armor', armorRouter());
 
 app.listen(port, () => {
     console.log(`Listening on ${port}`);
